921ad668107adaba756943a52e82f50f06c671df
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-02-12 23:03:07 +0100

    TestDiffs

---
+++ src/test/java/io/github/oliviercailloux/gitjfs/impl/GitDiffTests.java
@@ -0,0 +1,80 @@
+package io.github.oliviercailloux.gitjfs.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.UnmodifiableIterator;
+import io.github.oliviercailloux.gitjfs.GitFileSystem;
+import io.github.oliviercailloux.gitjfs.GitFileSystemProvider;
+import io.github.oliviercailloux.jgit.JGit;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tests about actual reading from a repo, using the Files API.
+ */
+public class GitDiffTests {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(GitDiffTests.class);
+
+	@Test
+	void testDiff() throws Exception {
+		try (DfsRepository repo = new InMemoryRepository(new DfsRepositoryDescription("myrepo"))) {
+			final ImmutableList<ObjectId> commits = JGit.createRepoWithSubDir(repo);
+			final GitFileSystemProvider provider = GitFileSystemProviderImpl.getInstance();
+			try (GitFileSystem gitFs = provider.newFileSystemFromDfsRepository(repo)) {
+				assertEquals(ImmutableSet.of(),
+						gitFs.diff(gitFs.getPathRoot(commits.get(0)), gitFs.getPathRoot(commits.get(0))));
+				{
+					final ImmutableSet<DiffEntry> diffs01 = gitFs.diff(gitFs.getPathRoot(commits.get(0)),
+							gitFs.getPathRoot(commits.get(1)));
+					final DiffEntry diff01 = Iterables.getOnlyElement(diffs01);
+					assertEquals(ChangeType.ADD, diff01.getChangeType());
+					assertEquals("file2.txt", diff01.getNewPath());
+				}
+				{
+					final ImmutableSet<DiffEntry> diffs10 = gitFs.diff(gitFs.getPathRoot(commits.get(1)),
+							gitFs.getPathRoot(commits.get(0)));
+					final DiffEntry diff10 = Iterables.getOnlyElement(diffs10);
+					assertEquals(ChangeType.DELETE, diff10.getChangeType());
+					assertEquals("file2.txt", diff10.getOldPath());
+				}
+				{
+					final ImmutableSet<DiffEntry> diffs12 = gitFs.diff(gitFs.getPathRoot(commits.get(1)),
+							gitFs.getPathRoot(commits.get(2)));
+					final UnmodifiableIterator<DiffEntry> iterator = diffs12.iterator();
+					final DiffEntry diff12first = iterator.next();
+					final DiffEntry diff12second = iterator.next();
+					assertEquals(ChangeType.ADD, diff12first.getChangeType());
+					assertEquals("dir/file.txt", diff12first.getNewPath());
+					assertEquals(ChangeType.MODIFY, diff12second.getChangeType());
+					assertEquals("file2.txt", diff12second.getOldPath());
+					assertEquals("file2.txt", diff12second.getNewPath());
+				}
+				{
+					final ImmutableSet<DiffEntry> diffs02 = gitFs.diff(gitFs.getPathRoot(commits.get(0)),
+							gitFs.getPathRoot(commits.get(2)));
+					final UnmodifiableIterator<DiffEntry> iterator = diffs02.iterator();
+					final DiffEntry diff02first = iterator.next();
+					final DiffEntry diff02second = iterator.next();
+					assertFalse(iterator.hasNext());
+					assertEquals(ChangeType.ADD, diff02first.getChangeType());
+					assertEquals("dir/file.txt", diff02first.getNewPath());
+					assertEquals(ChangeType.ADD, diff02second.getChangeType());
+					assertEquals("file2.txt", diff02second.getNewPath());
+				}
+			}
+		}
+	}
+
+}
