4ceca36fabd38c032ec8a1061f183ed5471edbf3
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-02-13 16:34:16 +0100

    Documented Ref specifics

--- src/main/java/io/github/oliviercailloux/gitjfs/GitPathRootRef.java
+++ src/main/java/io/github/oliviercailloux/gitjfs/GitPathRootRef.java
@@ -4,5 +4,23 @@ package io.github.oliviercailloux.gitjfs;
  * A {@link GitPathRoot} containing a git ref.
  */
 public interface GitPathRootRef extends GitPathRoot {
+  /**
+   * Returns {@code false}.
+   *
+   * @return {@code false}
+   * @deprecated No reason to call this.
+   */
+  @Override
+  @Deprecated
+  boolean isCommitId();
 
+  /**
+   * Returns {@code true}.
+   *
+   * @return {@code true}
+   * @deprecated No reason to call this.
+   */
+  @Override
+  @Deprecated
+  boolean isRef();
 }
