b3a772ea7bcc77f9c731581fc1e8367ed4bcb88b
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-01-15 21:14:17 +0100

    Wording

--- src/main/java/io/github/oliviercailloux/gitjfs/package-info.java
+++ src/main/java/io/github/oliviercailloux/gitjfs/package-info.java
@@ -4,7 +4,7 @@
  *
  * {@link java.nio.file.spi.FileSystemProvider#readSymbolicLink} does not document that it may throw
  * {@link java.nio.file.NoSuchFileException}, whereas this possible exception is mentioned in
- * similar methods. Is this a mistake? If not, what should this method throw when trying to read a
+ * similar methods. Is this an oversight? If not, what should this method throw when trying to read a
  * non existent path?
  * <p>
  * {@link java.nio.file.FileSystem#getRootDirectories()}: I have no idea why the Java Files API does
