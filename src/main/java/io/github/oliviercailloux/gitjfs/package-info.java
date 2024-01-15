388688ad94eefd722e4739f610f7af3212a04eba
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-01-16 00:08:28 +0100

    Checkstyle

--- src/main/java/io/github/oliviercailloux/gitjfs/package-info.java
+++ src/main/java/io/github/oliviercailloux/gitjfs/package-info.java
@@ -4,8 +4,8 @@
  *
  * {@link java.nio.file.spi.FileSystemProvider#readSymbolicLink} does not document that it may throw
  * {@link java.nio.file.NoSuchFileException}, whereas this possible exception is mentioned in
- * similar methods. Is this an oversight? If not, what should this method throw when trying to read a
- * non existent path?
+ * similar methods. Is this an oversight? If not, what should this method throw when trying to read
+ * a non existent path?
  * <p>
  * {@link java.nio.file.FileSystem#getRootDirectories()}: I have no idea why the Java Files API does
  * not want an IOException here.
